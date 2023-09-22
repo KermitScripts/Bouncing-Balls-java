3f23123ce1ebd0ce818c21149188a58b516681b7
KermitScripts <143460658+KermitScripts@users.noreply.github.com>
2023-09-22 18:10:45 +0200

    Update BallRunnable.java
    passing a list of balls into moveBall for collision detection.

--- src/bouncingBalls/BallRunnable.java
+++ src/bouncingBalls/BallRunnable.java
@@ -1,30 +1,30 @@
 package bouncingBalls;
 
-public class BallRunnable implements Runnable
-{
-  private Ball ball;
-  private BallComponent component;
-  private BallFrame frame;
-  private int speed;
+import java.util.ArrayList;
 
+public class BallRunnable implements Runnable {
+    private Ball ball;
+    private BallComponent component;
+    private BallFrame frame;
+    private int speed;
+    private ArrayList<Ball> balls; // Add a list to hold all balls
 
-   public BallRunnable(Ball b, BallComponent c, BallFrame f) {
-      ball = b;
-      component = c;
-      frame = f;
-   }
-
-  public void run() {
-  		try {
-        while (frame.getstopThreads() == false) {
-          speed = frame.setSpeed();
-          ball.moveBall(frame);
-          component.paintBalls();
-          Thread.sleep(speed);
-        }
-  		}
-  		catch (InterruptedException e){
+    public BallRunnable(Ball b, BallComponent c, BallFrame f, ArrayList<Ball> balls) {
+        ball = b;
+        component = c;
+        frame = f;
+        this.balls = balls; // Store the list of balls
     }
-  }
-}
 
+    public void run() {
+        try {
+            while (frame.getstopThreads() == false) {
+                speed = frame.setSpeed();
+                ball.moveBall(frame, balls);
+                component.paintBalls();
+                Thread.sleep(speed);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+}
